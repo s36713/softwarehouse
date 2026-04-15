0f04b925e8e8248d80a5fbff335af00739719639
s36713 <s36713@pjwstk.edu.pl>
2026-04-15 11:26:51 +0200

    utworzenie klasy "programista"

--- src/Programista.java
+++ src/Programista.java
@@ -1,11 +1,23 @@
 public class Programista extends pracownik {
     private String glownyjezyk;
-    private int liczbarepozytoiow;
+    private int liczbarepozytoriow;
 
-    public Programista(String idPracownika, String imie, String nazwisko, double stawkaBazowa, String glownyjezyk, int liczbarepozytoiow) {
+    public Programista(String idPracownika, String imie, String nazwisko, double stawkaBazowa, String glownyjezyk, int liczbarepozytoriow) {
         super(idPracownika, imie, nazwisko, stawkaBazowa);
         this.glownyjezyk = glownyjezyk;
-        this.liczbarepozytoiow = liczbarepozytoiow;
+        this.liczbarepozytoriow = liczbarepozytoriow;
     }
 
+    @Override
+    public double obliczKosztMiesieczny() {
+        return super.obliczKosztMiesieczny();
+    }
+    @Override
+    public String przedstawSie() {
+        return super.przedstawSie();
+    }
+    public void wypiszTechnologie(){
+        System.out.println("Glowny jezyk:" + glownyjezyk );
+        System.out.println("Liczba zarzadzanych repozytoriow" + liczbarepozytoriow);
+    }
 }
