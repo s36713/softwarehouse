c4bc076287f85027ebff3b02a280d8a235decf27
s36713 <s36713@pjwstk.edu.pl>
2026-04-15 11:03:50 +0200

    utworzenie klasy "pracownik"

--- src/pracownik.java
+++ src/pracownik.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+import java.util.Objects;
 
 public class pracownik {
+    private String idPracownika;
+    private String imie;
+    private String nazwisko;
+    private double stawkaBazowa;
+
+    public pracownik(String idPracownika, String imie, String nazwisko, double stawkaBazowa) {
+        this.idPracownika = idPracownika;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.stawkaBazowa = stawkaBazowa;
+    }
+
+    public final String pobierzIdPracownika() {
+        return idPracownika;
+    }
+
+    public double obliczKosztMiesieczny() {
+        return stawkaBazowa;
+
+    }
+
+    public String przedstawSie() {
+        return imie + " " + nazwisko;
+    }
+
+    @Override
+    public String toString() {
+        return "pracownik{" +
+                "idPracownika='" + idPracownika + '\'' +
+                ", imie='" + imie + '\'' +
+                ", nazwisko='" + nazwisko + '\'';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        pracownik pracownik = (pracownik) o;
+        return Objects.equals(idPracownika, pracownik.idPracownika);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(idPracownika);
+    }
+
 }
